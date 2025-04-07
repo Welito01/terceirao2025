189e419f79e4eb73249f85ff2b658b1c5847bb3d
Welito01 <163025971+Welito01@users.noreply.github.com>
2025-04-07 14:29:40 -0300

    Add files via upload
    07/04/2025
    Códigos da aula 8 e 9 copiados

--- script.js
+++ script.js
@@ -19,13 +19,15 @@ const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
 const alternaContraste = document.getElementById('alterna-contraste')
 
-alternaConstraste.addEventListener ('click', function(){
+alternaConstraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
-
 })
 
-const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+ScrollReveal().reveal ('#inicio', {delay:500});
+ScrollReveal().reveal ('#tropicalia', {delay:500});
+ScrollReveal().reveal ('#galeria', {delay:500});
+ScrollReveal().reveal ('#contatp', {delay:500});
+
 
 
     
